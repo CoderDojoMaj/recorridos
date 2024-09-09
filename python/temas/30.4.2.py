4bdd71683dcfe0bdeb19f0b67451bf5563af1e44
Santi <s@alvarezrojo.es>
2024-09-09 23:12:27 +0200

    9

---
+++ python/temas/30.4.2.py
@@ -0,0 +1,16 @@
+class Frigo:
+    def __init__(self,color, temperatura):
+        self.__color = color
+        self.fija_temperatura(temperatura)
+
+    def __str__(self):
+        return f"Color {self.__color} | {self.__temperatura}ºC"
+
+    def fija_temperatura(self,nueva_temperatura):
+        if nueva_temperatura < 0:
+            raise ValueError("no puede ser < 0")
+        self.__temperatura = nueva_temperatura
+
+
+f1 = Frigo("rojo", -1)
+print( f1 )
