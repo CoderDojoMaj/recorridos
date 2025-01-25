8f67e4d3eef3d57c462ffd8c1a145b42b7561b60
Santi <s@alvarezrojo.es>
2025-01-25 10:02:40 +0100

    25

--- POO/temas/17/py/17.py
+++
@@ -1,9 +0,0 @@
-edadTuya = int(input("dime tu edad: "))
-edadAmigo = int( input("dime la edad de tu amigo: "))
-
-if edadTuya > edadAmigo:
-    print( "Tienes más años más que tu amigo" )
-elif edadTuya < edadAmigo:
-    print( "Tu amigo tiene más años más que tú" )
-else:
-    print( "Tenéis la misma edad" )
